a568318a3899a28820d00e0cbcc8946b79113ef5
Joshua Mankelow <joshua@mankelow.org>
2023-12-02 21:14:15 +0000

    day 2 part 1 solution!

---
+++ day_02/src/bin/1.rs
@@ -0,0 +1,84 @@
+use std::collections::HashMap;
+
+fn main() {
+    let input = include_str!("input1.txt");
+    let result = calculate(input);
+    println!("result: {:?}", result);
+}
+
+fn calculate(input: &str) -> i32 {
+    let games = input.split("\n").collect::<Vec<&str>>();
+    let mut valid_games: Vec<i32> = Vec::new();
+    for game in games {
+        println!("============ NEW GAME ============");
+        println!("game: {}", game);
+        if let Some((game_id_str, rounds)) = game.split_once(": ") {
+            if let Some((_, id_str)) = game_id_str.split_once(" ") {
+                if let Ok(id) = id_str.parse::<i32>() {
+                    println!("id: {}", id);
+                    let largest_cubes = largest_cubes(rounds);
+                    // println!("max_cubes: {:?}", largest_cubes);
+                    let max_red = largest_cubes.get("red").unwrap();
+                    let max_green = largest_cubes.get("green").unwrap();
+                    let max_blue = largest_cubes.get("blue").unwrap();
+
+                    match (max_red <= &12, max_green <= &13, max_blue <= &14) {
+                        (true, true, true) => {
+                            valid_games.push(id as i32);
+                        }
+                        _ => { /* do nothing */ }
+                    }
+                }
+            }
+        } else {
+            println!("Unable to split game: {}", game);
+        }
+    }
+    return valid_games.iter().sum();
+}
+
+fn largest_cubes(game: &str) -> HashMap<&str, i32> {
+    let rounds: Vec<&str> = game.split("; ").collect();
+    println!("rounds: {:?}", rounds);
+    let mut max_cubes: HashMap<&str, i32> = HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
+
+    for round in rounds.iter() {
+        let cubes: Vec<&str> = round.split(", ").collect();
+        println!("cubes: {:?}", cubes);
+        for cube in cubes.iter() {
+            if let Some((number_str, colour)) = cube.split_once(" ") {
+                if let Ok(number) = number_str.parse() {
+                    if let Some(max_current_colour) = max_cubes.get(colour) {
+                        // println!("number: {}", number);
+                        // println!("colour: {}", colour);
+                        // println!("max_current_colour: {}", number);
+                        if number > *max_current_colour {
+                            max_cubes.insert(colour, number);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    return max_cubes;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn case1() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+
+        let result = calculate(
+            "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green",
+        );
+        assert_eq!(result, 8)
+    }
+}
---
+++ day_02/src/bin/2.rs
@@ -0,0 +1,16 @@
+fn main() {
+    println!("day_1 part 2!");
+}
+
+fn calculate(input: &str) -> String {
+    return "day 1 part 2".to_string();
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
